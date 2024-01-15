d8daf78cdbe306a513431525829f931a59e87a42
David Gelpi <gelpiorama@gmail.com>
2024-01-15 10:33:57 +0100

    refactor(__filter_pips_repeated, __biggest_pip_repeated): Encapsular el calculo del pip mayor repetido en biggest

--- src/yatzy.py
+++ src/yatzy.py
@@ -77,11 +77,11 @@ class Yatzy:
     @classmethod
     def __biggest_pip_repeated(cls, dice, times):
         pips = cls.__filter_pips_repeated(dice, times)
-        return pips[0] if pips else []
+        return pips[-1] if pips else []
 
     @classmethod
     def __filter_pips_repeated(cls, dice, times):
-        return list(filter(lambda pip: dice.count(pip) >= times, Pips.reversedValues()))
+        return list(filter(lambda pip: dice.count(pip) >= times, Pips.values()))
 
     @classmethod
     def small_straight(cls, *dice):
